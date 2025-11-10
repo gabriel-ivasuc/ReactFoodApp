68c4533edfb5a485cbc00d7325b94965673e8ec1
Gabriel Ivasuc <gabrielivasuc@yahoo.com>
2025-11-10 15:15:55 +0000

    added currency formatter file and used it on meal prices

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,14 +1,16 @@
-export default function MealItem({meal}) {
+import { currencyFormatter } from "../util/formatting";
+
+export default function MealItem({meal, onClick}) {
   return <li className='meal-item'>
     <article>
       <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
       <div>
         <h3>{meal.name}</h3>
-        <p className='meal-item-price'>{meal.price}</p>
+        <p className='meal-item-price'>{currencyFormatter.format(meal.price)}</p>
         <p className='meal-item-description'>{meal.description}</p>
       </div>
       <p className="meal-item-actions">
-        <button>Add meal to basket</button>
+        <button onClick={meal}>Add meal to basket</button>
       </p>
     </article>
   </li>
